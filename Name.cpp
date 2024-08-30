ccf82e515dae2a097fc8214bfe8b42e89eee26c9
wbyates777 <wbyates777@gmail.com>
2024-08-30 14:16:49 +0200

    Added clip functions

--- Name.cpp
+++ Name.cpp
@@ -1,4 +1,4 @@
-/* Name 20/01/2024
+/* Name 28/08/2024
 
  $$$$$$$$$$$$$$$$
  $   Name.cpp   $
@@ -8,15 +8,22 @@
  Copyright (c) W.B. Yates. All rights reserved.
  History:
 
- Helper class
+ Helper class used to process the names in MarketId, Currency, Country and City.
  
- Convert names with accents to 'ASCII' names by removing newlines, escaping single 
- quote ['] (note that [‘] is a different character) and mapping, in a transparent manner, 
- 'non-ASCII' characters such as:  à => a and á => a. This can be useful for generating search keys.
+ Process names with accents, by removing newlines, escaping single character 
+ quote ['] (NB not [`]) and mappimg, in a transparent manner, non-ASCII characters such as: à => a and á => a. 
+
+ Also provides methods to remove leading and trailing white space, quotes or brackets,
+ and to split strings by an arbitrary delimeter. 
  
- Also provides methods to split strings by arbitrary delimeter.
- 
- Used to process the names in Country and City.
+  
+  \a = \x07 = alert (bell)
+  \b = \x08 = backspace
+  \t = \x09 = horizonal tab
+  \n = \x0A = newline (or line feed)
+  \v = \x0B = vertical tab
+  \f = \x0C = form feed
+  \r = \x0D = carriage return
  
 */
 
@@ -25,22 +32,26 @@
 #include "Name.h"
 #endif
 
+#include <cassert>
 
-std::map<std::string, std::string> Name::m_match;
+// https://www.regular-expressions.info
+// https://www.regexlib.com/Default.aspx
 
+const std::regex Name::m_left_whitespace   = std::regex( R"(^\s*)" );                // WHITESPACE
+const std::regex Name::m_right_whitespace  = std::regex( R"(\s*$)" );
+const std::regex Name::m_left_quotes       = std::regex( R"(^\s*[`'"])" );           // QUOTES
+const std::regex Name::m_right_quotes      = std::regex( R"([`'"]\s*$)" );
 
-Name::Name( void )
-{
-    setup();
-}
+std::map<std::string, std::string> Name::m_accent;
+std::map<std::string, std::string> Name::m_escape;
 
 
 std::string
-Name::remove_accents( std::string str )
-// substitute characters with accents 
+Name::deaccent( std::string str )
+// substitute characters with accents -- slow
 {
     // const std::map<std::string, std::string>::value_type& c
-    for (auto &c : m_match)
+    for (auto &c : m_accent)
     {
         size_t pos = 0;
         while ((pos = str.find(c.first, pos)) != str.npos)
@@ -50,31 +61,43 @@ Name::remove_accents( std::string str )
     return str;
 }
 
-
 std::string
-Name::toASCII( const std:: string &str ) 
-// remove newlines and escape single quote ['] - note [‘] is a different character
+Name::escape( const std::string& str )
+// escape std::regex special characters 
 {
-    std::string retVal = remove_accents(str);
-
-    for (int k = 0; k < retVal.size(); ++k)
-    {
-        if (retVal[k] == '\'')
-            retVal.insert(k++, 1, '\\');
-        else if (retVal[k] == '\n')
-            retVal.erase(k);
-    }
+    std::string retVal;
     
+    for (char c : str)
+    {
+        std::string sym(1, c);
+        auto fitr = m_escape.find(sym);
+        retVal += (fitr != m_escape.end()) ? fitr->second : sym;
+    }
+
     return retVal;
 }
 
+std::string
+Name::denewln( std::string str )
+// remove newln '\n' and carriage return '\r', and escape single quote '\''
+{
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == '\'')
+            str.insert(i++, 1, '\\');
+        else if (str[i] == '\n' || str[i] == '\r')
+            str.erase(i);
+    }
+    
+    return str;
+}
 
 std::vector<std::string> 
-Name::split( const std::string &str, const std::string &delim, int num ) 
+Name::split( const std::string &str, const std::string &delim ) 
 {
     std::vector<std::string> retVal;
-    retVal.reserve(num);
 
+    // we don't need to escape(delim) as we search using std::string find
     size_t pos_start = 0, pos_end = 0, delim_len = delim.length();
     while ((pos_end = str.find(delim, pos_start)) != std::string::npos) 
     {
@@ -86,23 +109,33 @@ Name::split( const std::string &str, const std::string &delim, int num )
     return retVal;    
 }
 
-
 std::vector<std::string>
-Name::split( std::string str, const std::regex &delim, int num ) 
+Name::split( std::string str, const std::regex &delim ) 
 {
     std::vector<std::string> retVal;
-    retVal.reserve(num);
 
     std::smatch match;
     while (std::regex_search(str, match, delim))
     {
-        if (!match.empty())
-        {
-            retVal.push_back( match.prefix() );
-            str = match.suffix();   
-        }
+        retVal.push_back( match.prefix() );
+        str = match.suffix();   
     }
     retVal.push_back( str );
+
+    return retVal;
+}
+
+std::string 
+Name::capitalise( const std::string &str )
+{
+    std::string retVal;
+
+    char last = ' ';
+    for (char c : str)
+    {
+        retVal += (last == ' ' && c != ' ') ? std::toupper(c) : std::tolower(c);
+        last = c;
+    }
 
     return retVal;
 }
@@ -110,216 +143,245 @@ Name::split( std::string str, const std::regex &delim, int num )
 
 void
 Name::setup( void )
-// add/delete mappings as you see fit
 // alphabets with accents to watch out for include 
 // Romanian, Polish, Vietnamese, and Cyrillic.
+// https://www.fileformat.info/info/charset/UTF-16/list.htm
+// https://www.codetable.net/unicodecharacters
 {
-    m_match["Æ"] = "AE";
-    m_match["æ"] = "ae";
+    //
+    // std::regex special characters
+    //
+    
+    m_escape["."]  = R"(\.)";
+    m_escape["\\"] = R"(\\)";
+    m_escape["+"]  = R"(\+)";
+    m_escape["*"]  = R"(\*)";
+    m_escape["?"]  = R"(\?)";
+    m_escape["["]  = R"(\[)";
+    m_escape["^"]  = R"(\^)";
+    m_escape["]"]  = R"(\])";
+    m_escape["$"]  = R"(\$)";
+    m_escape["("]  = R"(\()";
+    m_escape[")"]  = R"(\))";
+    m_escape["{"]  = R"(\{)";
+    m_escape["}"]  = R"(\})";
+    m_escape["="]  = R"(\=)";
+    m_escape["!"]  = R"(\!)";
+    m_escape["<"]  = R"(\<)";
+    m_escape[">"]  = R"(\>)";
+    m_escape["|"]  = R"(\|)";
+    m_escape[":"]  = R"(\:)";
+    m_escape["-"]  = R"(\-)";
+    
+    //
+    // accents and their replacements
+    //
+    
+    // NB never have the same character in the key and the value
+    // i.e m_accent["'"] = R"(\')";
+    // this will loop forever as "'" is matched again and again
+    
+    m_accent["Æ"] = "AE";
+    m_accent["æ"] = "ae";
 
-    m_match["Œ"] = "OE";
-    m_match["œ"] = "oe";
+    m_accent["Œ"] = "OE";
+    m_accent["œ"] = "oe";
     
-    m_match["ß"] = "ss";
-    m_match["Þ"] = "th";
+    m_accent["ß"] = "ss";
     
-    m_match["Ā"] = "A"; // Latin A with macron
-    m_match["Á"] = "A"; // Latin A with acute
-    m_match["À"] = "A"; // Latin A with grave
-    m_match["Ã"] = "A"; // Latin A with tilde
-    m_match["Â"] = "A"; // Latin A with circumflex
-    m_match["Ä"] = "A"; // Latin A with diaeresis
-    m_match["Å"] = "A"; // Latin A with ring above
-    m_match["Ă"] = "A"; // Latin A with breve
-    m_match["Ą"] = "A"; // Latin A with ogonek
+    m_accent["Þ"] = "Th";
+    m_accent["þ"] = "th";
+ 
+    m_accent["Ā"] = "A"; // Latin A with macron
+    m_accent["Á"] = "A"; // Latin A with acute
+    m_accent["À"] = "A"; // Latin A with grave
+    m_accent["Ã"] = "A"; // Latin A with tilde
+    m_accent["Â"] = "A"; // Latin A with circumflex
+    m_accent["Ä"] = "A"; // Latin A with diaeresis
+    m_accent["Å"] = "A"; // Latin A with ring above
+    m_accent["Ă"] = "A"; // Latin A with breve
+    m_accent["Ą"] = "A"; // Latin A with ogonek
     
-    m_match["Č"] = "C";
-    m_match["Ç"] = "C";
+    m_accent["Č"] = "C";
+    m_accent["Ç"] = "C";
     
-    m_match["Ḑ"] = "D"; 
-    m_match["Đ"] = "D"; 
+    m_accent["Ḑ"] = "D"; 
+    m_accent["Đ"] = "D"; 
     
-    m_match["É"] = "E"; 
-    m_match["È"] = "E";
+    m_accent["É"] = "E"; 
+    m_accent["È"] = "E";
     
-    m_match["Ħ"] = "H"; 
-    m_match["Ḩ"] = "H";
+    m_accent["Ħ"] = "H"; 
+    m_accent["Ḩ"] = "H";
     
-    m_match["Í"] = "I"; 
-    m_match["Ì"] = "I";
-    m_match["İ"] = "I";  
-    m_match["Ï"] = "I";
-    m_match["Ī"] = "I";
-    m_match["Î"] = "I";
+    m_accent["Í"] = "I"; 
+    m_accent["Ì"] = "I";
+    m_accent["İ"] = "I";  
+    m_accent["Ï"] = "I";
+    m_accent["Ī"] = "I";
+    m_accent["Î"] = "I";
     
-    m_match["Ñ"] = "N"; 
+    m_accent["Ñ"] = "N"; 
     
-    m_match["Ò"] = "O";
-    m_match["Ó"] = "O";
-    m_match["Ô"] = "O";
-    m_match["Õ"] = "O";
-    m_match["Ö"] = "O"; 
-    m_match["Ø"] = "O";
+    m_accent["Ò"] = "O";
+    m_accent["Ó"] = "O";
+    m_accent["Ô"] = "O";
+    m_accent["Õ"] = "O";
+    m_accent["Ö"] = "O"; 
+    m_accent["Ø"] = "O";
     
-    m_match["Ķ"] = "K";
+    m_accent["Ķ"] = "K";
     
-    m_match["Ł"] = "L";
+    m_accent["Ł"] = "L";
     
-    m_match["Š"] = "S";
-    m_match["Ş"] = "S"; 
-    m_match["Ś"] = "S"; 
-    m_match["Ș"] = "S";
+    m_accent["Š"] = "S";
+    m_accent["Ş"] = "S"; 
+    m_accent["Ś"] = "S"; 
+    m_accent["Ș"] = "S";
     
-    m_match["Ţ"] = "T";
-    m_match["Ť"] = "T";
-    m_match["Ŧ"] = "T";
-    m_match["Ƭ"] = "T";
-    m_match["Ʈ"] = "T";
+    m_accent["Ţ"] = "T";
+    m_accent["Ť"] = "T";
+    m_accent["Ŧ"] = "T";
+    m_accent["Ƭ"] = "T";
+    m_accent["Ʈ"] = "T";
     
-    m_match["Ú"] = "U";
-    m_match["Ù"] = "U";
-    m_match["Û"] = "U";
-    m_match["Ü"] = "U";
+    m_accent["Ú"] = "U";
+    m_accent["Ù"] = "U";
+    m_accent["Û"] = "U";
+    m_accent["Ü"] = "U";
     
-    m_match["Ż"] = "Z";  
-    m_match["Z̧"] = "Z"; 
-    m_match["Ž"] = "Z";
-    m_match["Ƶ"] = "Z";
-    m_match["Ž"] = "Z";
-    m_match["Ź"] = "Z";
-    m_match["Ȥ"] = "Z";
+    m_accent["Ż"] = "Z";  
+    m_accent["Z̧"] = "Z"; 
+    m_accent["Ž"] = "Z";
+    m_accent["Ƶ"] = "Z";
+    m_accent["Ž"] = "Z";
+    m_accent["Ź"] = "Z";
+    m_accent["Ȥ"] = "Z";
     
     //
     //
     //
     
-    m_match["à"] = "a";
-    m_match["á"] = "a"; 
-    m_match["â"] = "a";
-    m_match["ã"] = "a";
-    m_match["ä"] = "a";
-    m_match["å"] = "a";
-    m_match["ả"] = "a"; 
-    m_match["ậ"] = "a";
-    m_match["ằ"] = "a"; 
-    m_match["ắ"] = "a"; 
-    m_match["ā"] = "a"; 
-    m_match["ą"] = "a";   
-    m_match["ă"] = "a";
-    m_match["ầ"] = "a";
-    m_match["ẵ"] = "a";
-    m_match["ạ"] = "a";
+    m_accent["à"] = "a";
+    m_accent["á"] = "a"; 
+    m_accent["â"] = "a";
+    m_accent["ã"] = "a";
+    m_accent["ä"] = "a";
+    m_accent["å"] = "a";
+    m_accent["ả"] = "a"; 
+    m_accent["ậ"] = "a";
+    m_accent["ằ"] = "a"; 
+    m_accent["ắ"] = "a"; 
+    m_accent["ā"] = "a"; 
+    m_accent["ą"] = "a";   
+    m_accent["ă"] = "a";
+    m_accent["ầ"] = "a";
+    m_accent["ẵ"] = "a";
+    m_accent["ạ"] = "a";
     
-    m_match["ç"] = "c"; 
-    m_match["ć"] = "c";
-    m_match["č"] = "c"; 
-    m_match["ċ"] = "c"; 
-    m_match["ĉ"] = "c";
-    m_match["ƈ"] = "c";
+    m_accent["ç"] = "c"; 
+    m_accent["ć"] = "c";
+    m_accent["č"] = "c"; 
+    m_accent["ċ"] = "c"; 
+    m_accent["ĉ"] = "c";
+    m_accent["ƈ"] = "c";
     
-    m_match["ď"] = "d";
-    m_match["ḑ"] = "d";
-    m_match["đ"] = "d";
+    m_accent["ď"] = "d";
+    m_accent["ḑ"] = "d";
+    m_accent["đ"] = "d";
     
-    m_match["é"] = "e";   
-    m_match["è"] = "e"; 
-    m_match["ė"] = "e";
-    m_match["ë"] = "e"; 
-    m_match["ế"] = "e";
-    m_match["ề"] = "e";
-    m_match["ě"] = "e";
-    m_match["ê"] = "e";
-    m_match["ệ"] = "e";
-    m_match["ę"] = "e";
-    m_match["ē"] = "e";
-    m_match["ə"] = "e";
+    m_accent["é"] = "e";   
+    m_accent["è"] = "e"; 
+    m_accent["ė"] = "e";
+    m_accent["ë"] = "e"; 
+    m_accent["ế"] = "e";
+    m_accent["ề"] = "e";
+    m_accent["ě"] = "e";
+    m_accent["ê"] = "e";
+    m_accent["ệ"] = "e";
+    m_accent["ę"] = "e";
+    m_accent["ē"] = "e";
+    m_accent["ə"] = "e";
     
-    m_match["ġ"] = "g";
-    m_match["ğ"] = "g"; 
-    m_match["ĝ"] = "g";
-    m_match["ģ"] = "g";
+    m_accent["ġ"] = "g";
+    m_accent["ğ"] = "g"; 
+    m_accent["ĝ"] = "g";
+    m_accent["ģ"] = "g";
     
-    m_match["ḩ"] = "h";           
-    m_match["ḥ"] = "h";
-    m_match["ħ"] = "h"; 
-    m_match["ĥ"] = "h";
+    m_accent["ḩ"] = "h";           
+    m_accent["ḥ"] = "h";
+    m_accent["ħ"] = "h"; 
+    m_accent["ĥ"] = "h";
     
-    m_match["í"] = "i"; 
-    m_match["ì"] = "i";
-    m_match["ĩ"] = "i";
-    m_match["î"] = "i"; 
-    
-    m_match["ĭ"] = "i";
-    m_match["ī"] = "i";
-    m_match["ı"] = "i";
-    m_match["ï"] = "i";
-    m_match["ị"] = "i";
+    m_accent["í"] = "i"; 
+    m_accent["ì"] = "i";
+    m_accent["ĩ"] = "i";
+    m_accent["î"] = "i"; 
+    m_accent["ĭ"] = "i";
+    m_accent["ī"] = "i";
+    m_accent["ı"] = "i";
+    m_accent["ï"] = "i";
+    m_accent["ị"] = "i";
 
-    m_match["ł"] = "l";
+    m_accent["ł"] = "l";
     
-    m_match["ñ"] = "n";
-    m_match["ň"] = "n";
-    m_match["ń"] = "n";
-    m_match["ņ"] = "n";
+    m_accent["ñ"] = "n";
+    m_accent["ň"] = "n";
+    m_accent["ń"] = "n";
+    m_accent["ņ"] = "n";
     
-    m_match["ồ"] = "o"; 
-    m_match["ó"] = "o";
-    m_match["ò"] = "o"; 
-    m_match["ö"] = "o";
-    m_match["ǒ"] = "o";     
-    m_match["ô"] = "o";
-    m_match["ð"] = "o"; 
-    m_match["õ"] = "o";
-    m_match["ő"] = "o";
-    m_match["ọ"] = "o";
-    m_match["ơ"] = "o"; 
-    m_match["ō"] = "o";
-    m_match["ộ"] = "o"; 
-    m_match["ớ"] = "o";
-    m_match["ø"] = "o";
-    m_match["ǿ"] = "o";
+    m_accent["ồ"] = "o"; 
+    m_accent["ó"] = "o";
+    m_accent["ò"] = "o"; 
+    m_accent["ö"] = "o";
+    m_accent["ǒ"] = "o";     
+    m_accent["ô"] = "o";
+    m_accent["ð"] = "o"; 
+    m_accent["õ"] = "o";
+    m_accent["ő"] = "o";
+    m_accent["ọ"] = "o";
+    m_accent["ơ"] = "o"; 
+    m_accent["ō"] = "o";
+    m_accent["ộ"] = "o"; 
+    m_accent["ớ"] = "o";
+    m_accent["ø"] = "o";
+    m_accent["ǿ"] = "o";
     
-    m_match["ṟ"] = "r";
-    m_match["ṙ"] = "r"; 
-    m_match["ř"] = "r";     
+    m_accent["ṟ"] = "r";
+    m_accent["ṙ"] = "r"; 
+    m_accent["ř"] = "r";     
     
-    m_match["š"] = "s"; 
-    m_match["ş"] = "s";
-    m_match["ś"] = "s";
-    m_match["ŝ"] = "s"; 
-    m_match["ș"] = "s";
+    m_accent["š"] = "s"; 
+    m_accent["ş"] = "s";
+    m_accent["ś"] = "s";
+    m_accent["ŝ"] = "s"; 
+    m_accent["ș"] = "s";
     
-    m_match["ţ"] = "t"; 
-    m_match["ț"] = "t";
+    m_accent["ţ"] = "t"; 
+    m_accent["ț"] = "t";
     
-    m_match["ů"] = "u";
-    m_match["ừ"] = "u"; 
-    m_match["ú"] = "u";
-    m_match["ū"] = "u";   
-    m_match["ü"] = "u";
-    m_match["ŭ"] = "u";
-    m_match["ũ"] = "u"; 
-    m_match["û"] = "u";
-    m_match["ư"] = "u";
+    m_accent["ů"] = "u";
+    m_accent["ừ"] = "u"; 
+    m_accent["ú"] = "u";
+    m_accent["ū"] = "u";   
+    m_accent["ü"] = "u";
+    m_accent["ŭ"] = "u";
+    m_accent["ũ"] = "u"; 
+    m_accent["û"] = "u";
+    m_accent["ư"] = "u";
     
-    m_match["ý"] = "y";
-    m_match["ÿ"] = "y";
+    m_accent["ý"] = "y";
+    m_accent["ÿ"] = "y";
     
-    m_match["ż"] = "z"; 
-    m_match["ẕ"] = "z";
-    m_match["ž"] = "z"; 
-    m_match["ź"] = "z"; 
-    m_match["ż"] = "z";
-    m_match["z̧"] = "z";
-    m_match["ƶ"] = "z";
+    m_accent["ż"] = "z"; 
+    m_accent["ẕ"] = "z";
+    m_accent["ž"] = "z"; 
+    m_accent["ź"] = "z"; 
+    m_accent["ż"] = "z";
+    m_accent["z̧"] = "z";
+    m_accent["ƶ"] = "z";
 }
 
 
 //
-
-
-
-
-
-
 
